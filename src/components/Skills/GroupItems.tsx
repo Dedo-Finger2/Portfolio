e3bfbed53e0e782cb9871b90100fc090f355902e
Antonio Mauricio <antonioimportant@gmail.com>
2025-02-22 10:37:11 -0300

    Adds support for SM break point

--- src/components/Skills/GroupItems.tsx
+++ src/components/Skills/GroupItems.tsx
@@ -6,6 +6,8 @@ interface SkillGroupItemsProps {
 
 export default function SkillGroupItems({ children }: SkillGroupItemsProps) {
   return (
-    <div className="grid grid-cols-3 place-items-center gap-4">{children}</div>
+    <div className="grid grid-cols-3 place-items-center gap-4 sm:grid-cols-4 sm:place-items-start">
+      {children}
+    </div>
   );
 }
