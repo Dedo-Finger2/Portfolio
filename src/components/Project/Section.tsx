7926399543fbe123a9aa6e49782f59067ae2db7e
Antonio Mauricio <antonioimportant@gmail.com>
2025-02-22 10:19:59 -0300

    Uses Card component in ProjectSection

--- src/components/Project/Section.tsx
+++ src/components/Project/Section.tsx
@@ -1,54 +1,42 @@
-import ProjectCard from "../ProjectCard";
+import Card from "../Card/Section";
+import CardInfo from "../Card/Info";
+import CardTitle from "../Card/Title";
+import CardDescription from "../Card/Description";
+import CardTimeFrame from "../Card/TimeFrame";
+import CardActivitiesList from "../Card/ActivitiesList";
+import CardSkillsList from "../Card/SkillsList";
+
+import ProjectUrlList from "./UrlList";
+
 import RightSection from "../RightSection/Section";
 import RightSectionTitle from "../RightSection/Title";
+
 import SkillItem from "../Skills/Item";
+import Image from "../Image";
 
 export default function Section() {
   return (
     <RightSection>
       <RightSectionTitle>Projetos</RightSectionTitle>
-      <div className="flex flex-col gap-12">
-        <ProjectCard
-          title="Análise e Desenvolvimento de Sistemas"
-          imgSrc="https://loremflickr.com/200/200?random=1"
-          description="SENAI CIMATEC, Bahia, Camaçari"
-          timeFrame="Agosto 2022 - Dezembro 2023"
-          repoUrl="a"
-          demoUrl="b"
-          activities={
-            <>
-              <li>Teste</li>
-            </>
-          }
-          skillsUsed={
-            <>
-              <SkillItem bgColor={"bg-red-400/40"} textColor={"text-red-300"}>
-                Typescript
-              </SkillItem>
-            </>
-          }
-        />
-        <ProjectCard
-          title="Análise e Desenvolvimento de Sistemas"
-          imgSrc="https://loremflickr.com/200/200?random=1"
-          description="SENAI CIMATEC, Bahia, Camaçari"
-          timeFrame="Agosto 2022 - Dezembro 2023"
-          repoUrl="a"
-          demoUrl="b"
-          activities={
-            <>
-              <li>Teste</li>
-            </>
-          }
-          skillsUsed={
-            <>
-              <SkillItem bgColor={"bg-red-400/40"} textColor={"text-red-300"}>
-                Typescript
-              </SkillItem>
-            </>
-          }
-        />
-      </div>
+      <Card>
+        <Image src="https://picsum.photos/200" />
+
+        <CardInfo>
+          <CardTitle>Análise e Desenvolvimento de Sistemas </CardTitle>
+          <CardDescription>SENAI CIMATEC, BAHIA, Camaçari</CardDescription>
+          <CardTimeFrame>Agosto 2022 - Dezembro 2023</CardTimeFrame>
+        </CardInfo>
+
+        <CardActivitiesList>
+          <li>Opa</li>
+        </CardActivitiesList>
+
+        <CardSkillsList>
+          <SkillItem color="red">TypeScript</SkillItem>
+        </CardSkillsList>
+
+        <ProjectUrlList repositoryUrl="" demoUrl="a" />
+      </Card>
     </RightSection>
   );
 }
