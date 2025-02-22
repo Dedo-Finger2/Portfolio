8419089f95c60b111dcca416266517661e12399e
Antonio Mauricio <antonioimportant@gmail.com>
2025-02-22 10:19:47 -0300

    Adds UrlList for Project Components

---
+++ src/components/Project/UrlList.tsx
@@ -0,0 +1,33 @@
+import { FaGithub, FaExternalLinkAlt } from "react-icons/fa";
+
+interface Props {
+  repositoryUrl: string;
+  demoUrl?: string;
+}
+
+export default function ProjectUrlList({ repositoryUrl, demoUrl }: Props) {
+  return (
+    <div className="flex justify-between gap-4">
+      <a
+        href={repositoryUrl}
+        target="_blank"
+        className="flex gap-2 items-center text-sm"
+      >
+        <FaGithub />
+        Código Fonte
+      </a>
+      {demoUrl ? (
+        <a
+          href={demoUrl}
+          target="_blank"
+          className="flex gap-2 items-center text-sm"
+        >
+          <FaExternalLinkAlt />
+          Demonstração
+        </a>
+      ) : (
+        ""
+      )}
+    </div>
+  );
+}
