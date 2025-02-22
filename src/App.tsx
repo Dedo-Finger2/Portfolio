5f82255b3cf98681e17294c58197d525760bf213
Antonio Mauricio <antonioimportant@gmail.com>
2025-02-22 10:33:08 -0300

    Adds support for SM break point

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import ProjectsSection from "./components/Project/Section";
 
 export default function App() {
   return (
-    <div className="flex flex-col px-6 py-6 gap-6 mb-6">
+    <div className="flex flex-col px-6 py-6 gap-24 mb-6 sm:px-12 sm:py-12">
       {/* Left Side (Quick info) */}
       <QuickAboutMe />
       {/* Right Side (Large info) */}
