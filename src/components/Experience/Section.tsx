2a64442b12edacd190b90b0a207c0f5e365952e0
Antonio Mauricio <antonioimportant@gmail.com>
2025-02-22 10:14:15 -0300

    Refactors Experiences to use the new Card component

--- src/components/Experience/Section.tsx
+++ src/components/Experience/Section.tsx
@@ -1,32 +1,46 @@
-import ExperienceCard from "../ExperienceCard";
+import Card from "../Card/Section";
+import CardInfo from "../Card/Info";
+import CardTitle from "../Card/Title";
+import CardDescription from "../Card/Description";
+import CardTimeFrame from "../Card/TimeFrame";
+import CardActivitiesList from "../Card/ActivitiesList";
+import CardSkillsList from "../Card/SkillsList";
+
 import RightSection from "../RightSection/Section";
 import RightSectionTitle from "../RightSection/Title";
+
 import SkillItem from "../Skills/Item";
+import Hashtag from "../Hashtag";
+import Image from "../Image";
 
 export default function Section() {
   return (
     <RightSection>
       <RightSectionTitle>Experiências</RightSectionTitle>
-      <div className="flex flex-col gap-12">
-        <ExperienceCard
-          title="Análise e Desenvolvimento de Sistemas"
-          imgSrc="https://loremflickr.com/200/200?random=1"
-          description="SENAI CIMATEC, Bahia, Camaçari"
-          timeFrame="Agosto 2022 - Dezembro 2023"
-          activities={
-            <>
-              <li>Teste</li>
-            </>
-          }
-          skillsUsed={
-            <>
-              <SkillItem bgColor={"bg-red-400/40"} textColor={"text-red-300"}>
-                Typescript
-              </SkillItem>
-            </>
-          }
-        />
-      </div>
+      <Card>
+        <Image src="https://picsum.photos/201" />
+
+        <CardInfo>
+          <CardTitle>
+            Análise e Desenvolvimento de Sistemas{" "}
+            <Hashtag
+              text={"Técnico"}
+              bgColor={"bg-zinc-400/40"}
+              textColor={"text-zinc-400"}
+            />
+          </CardTitle>
+          <CardDescription>SENAI CIMATEC, BAHIA, Camaçari</CardDescription>
+          <CardTimeFrame>Agosto 2022 - Dezembro 2023</CardTimeFrame>
+        </CardInfo>
+
+        <CardActivitiesList>
+          <li>Opa</li>
+        </CardActivitiesList>
+
+        <CardSkillsList>
+          <SkillItem color="red">TypeScript</SkillItem>
+        </CardSkillsList>
+      </Card>
     </RightSection>
   );
 }
