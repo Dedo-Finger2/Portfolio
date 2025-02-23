b41469ed330edb44155f1bab7376e2f77b49575c
Antonio Mauricio <antonioimportant@gmail.com>
2025-02-23 08:46:55 -0300

    Animates QuickAboutMe

--- src/components/QuickAboutMe.tsx
+++ src/components/QuickAboutMe.tsx
@@ -8,6 +8,7 @@ import {
 import { IoMail } from "react-icons/io5";
 
 import mySelfie from "./../assets/minha-foto.jpg";
+import { motion } from "motion/react";
 
 export default function QuickAboutMe() {
   return (
@@ -16,43 +17,119 @@ export default function QuickAboutMe() {
         {/* Image + Personal info */}
         <div className="flex flex-col gap-4 items-center sm:items-start">
           {/* My Image */}
-          <img
+          <motion.img
+            initial={{ opacity: 0, x: -30 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ bounce: 0 }}
             className="shadow-lg shadow-zinc-600 rounded-full size-32 h-auto"
             src={mySelfie}
             alt="my-selfie"
           />
           {/* My name + What I do */}
           <div className="flex flex-col gap-2">
-            <h1 className="text-2xl font-bold text-center sm:text-left">
+            <motion.h1
+              initial={{ opacity: 0, y: 10, filter: "blur(10px)" }}
+              animate={{ opacity: 1, y: 0, filter: "none" }}
+              transition={{ bounce: 0, delay: 0.3 }}
+              className="text-2xl font-bold text-center sm:text-left"
+            >
               Antonio Mauricio de Almeida Neto
-            </h1>
-            <h2 className="text-zinc-400 font-semibold text-center sm:text-left">
+            </motion.h1>
+            <motion.h2
+              initial={{ opacity: 0, y: 10, filter: "blur(10px)" }}
+              animate={{ opacity: 1, y: 0, filter: "none" }}
+              transition={{ bounce: 0, delay: 0.5 }}
+              className="text-zinc-400 font-semibold text-center sm:text-left"
+            >
               Full-Stack Developer
-            </h2>
+            </motion.h2>
           </div>
         </div>
         {/* Where I live */}
         <div className="flex items-center gap-2 text-zinc-200 font-medium justify-center sm:justify-start">
-          <FaLocationDot />
-          <h3>Camaçari, BA - Brasil</h3>
+          <motion.div
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: 0.8, type: "spring" }}
+          >
+            <FaLocationDot />
+          </motion.div>
+          <motion.h3
+            initial={{ opacity: 0, y: -10, filter: "blur(10px)" }}
+            animate={{ opacity: 1, y: 0, filter: "none" }}
+            transition={{ bounce: 0, delay: 0.8 }}
+          >
+            Camaçari, BA - Brasil
+          </motion.h3>
         </div>
       </div>
       {/* Quick action sentence */}
-      <span className="text-xs text-center text-zinc-300 sm:text-left">
+      <motion.span
+        initial={{ opacity: 0, y: 10, filter: "blur(10px)" }}
+        animate={{ opacity: 1, y: 0, filter: "none" }}
+        transition={{ bounce: 0, delay: 1.2 }}
+        className="text-xs text-center text-zinc-300 sm:text-left"
+      >
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse, ea.
-      </span>
+      </motion.span>
       <div className="flex flex-col justify-center items-center gap-6 sm:items-start">
         {/* Download resume button */}
-        <button className="flex gap-2 items-center px-4 py-1 rounded-md border border-zinc-200 bg-zinc-800">
-          <FaFileDownload />
-          Currículo
-        </button>
+        <motion.div
+          initial={{ opacity: 0, x: -10 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ bounce: 0, delay: 1.1 }}
+        >
+          <motion.button
+            whileHover={{ scale: 1.05 }}
+            whileTap={{ scale: 1 }}
+            className="flex gap-2 items-center px-4 py-1 rounded-md border border-zinc-200 bg-zinc-800 hover:cursor-pointer"
+          >
+            <FaFileDownload />
+            Currículo
+          </motion.button>
+        </motion.div>
         {/* Social media */}
         <div className="flex gap-4 text-zinc-300">
-          <FaGithub size={20} />
-          <FaLinkedin size={20} />
-          <FaInstagram size={20} />
-          <IoMail size={20} />
+          <motion.div
+            initial={{ opacity: 0, x: -10 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ bounce: 0, delay: 1.2 }}
+          >
+            <FaGithub
+              className="hover:scale-125 transition-transform ease-in-out"
+              size={20}
+            />
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, x: -20 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ bounce: 0, delay: 1.4 }}
+          >
+            <FaLinkedin
+              className="hover:scale-125 transition-transform ease-in-out"
+              size={20}
+            />
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, x: -30 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ bounce: 0, delay: 1.6 }}
+          >
+            <FaInstagram
+              className="hover:scale-125 transition-transform ease-in-out"
+              size={20}
+            />
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, x: -50 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ bounce: 0, delay: 1.8 }}
+          >
+            <IoMail
+              className="hover:scale-125 transition-transform ease-in-out"
+              size={20}
+            />
+          </motion.div>
         </div>
       </div>
     </div>
