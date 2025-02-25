77b4c37ba470c0cdd52f22a6464c09c4e5590efc
Antonio Mauricio <antonioimportant@gmail.com>
2025-02-25 12:13:34 -0300

    Adds whileInView to the text + put some text in about me

--- src/components/AboutMe/Section.tsx
+++ src/components/AboutMe/Section.tsx
@@ -1,17 +1,43 @@
 import RightSection from "../RightSection/Section";
 import RightSectionTitle from "../RightSection/Title";
 
+import { motion } from "motion/react";
+
 export default function Section() {
   return (
     <RightSection>
       <RightSectionTitle>Sobre mim</RightSectionTitle>
       <p className="text-zinc-200 text-sm/6">
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae nisi hic
-        impedit inventore soluta saepe modi assumenda alias neque molestiae
-        optio qui nostrum eligendi laudantium adipisci necessitatibus totam,
-        exercitationem eos autem rerum ipsam. Mollitia, modi. Laudantium impedit
-        aliquid molestias illo fuga, ratione facere doloribus quis cumque enim
-        repellendus totam quod.
+        <motion.span
+          initial={{ opacity: 0, x: -100 }}
+          exit={{ opacity: 0, x: -100 }}
+          whileInView={{ opacity: 1, x: 0, transition: { delay: 0.2 } }}
+        >
+          Desenvolvedor web com 2 anos de experiência, especializado em
+          JavaScript
+        </motion.span>{" "}
+        <motion.span
+          initial={{ opacity: 0, x: -100 }}
+          whileInView={{ opacity: 1, x: 0, transition: { delay: 0.3 } }}
+          transition={{ delay: 0.3 }}
+        >
+          e tecnologias modernas. Comprometido em criar soluções eficientes,
+          criativas
+        </motion.span>{" "}
+        <motion.span
+          initial={{ opacity: 0, x: -100 }}
+          whileInView={{ opacity: 1, x: 0, transition: { delay: 0.4 } }}
+          transition={{ delay: 0.4 }}
+        >
+          e impactantes. Busco uma oportunidade para agregar valor à
+        </motion.span>{" "}
+        <motion.span
+          initial={{ opacity: 0, x: -100 }}
+          whileInView={{ opacity: 1, x: 0, transition: { delay: 0.5 } }}
+          transition={{ delay: 0.5 }}
+        >
+          empresa por meio do código limpo e inovação constante.
+        </motion.span>
       </p>
     </RightSection>
   );
