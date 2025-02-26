0518d72d8b5577064d578279864306ada7e69c47
Antonio Mauricio <antonioimportant@gmail.com>
2025-02-26 16:40:57 -0300

    Adds a transparent background to the images + makes the image contains + removes h-40 when it's shape is set to square

--- src/components/Image.tsx
+++ src/components/Image.tsx
@@ -6,7 +6,7 @@ export default function Image({ shape, ...props }: Props) {
   return (
     <img
       {...props}
-      className={`flex h-20 object-cover rounded-md shadow-md ${shape === "square" ? "sm:rounded-sm sm:h-40" : "sm:rounded-full"} sm:self-start sm:h-25`}
+      className={`flex h-20 object-contain bg-zinc-400/5 p-2 max-w-26 rounded-md shadow-md ${shape === "square" ? "sm:rounded-sm" : "sm:rounded-full"} sm:self-start sm:h-25`}
     />
   );
 }
